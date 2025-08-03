535a77ec47b5332bb8640bfbc9c4ab8199f6e62a
pranay7979 <pranayingle7979@gmail.com>
2025-08-04 00:46:50 +0530

    key leak errors fix

--- B/db.js
+++ B/db.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 async function connectToMongo() {
     try {
-        await mongoose.connect('mongodb+srv://pranay:pushpaingle@samaranify.p56gocr.mongodb.net/');
+        await mongoose.connect(MONGODB_URI);
         console.log('Successfully connected to MongoDB');
     } catch (err) {
         console.error('Error connecting to MongoDB:', err);
