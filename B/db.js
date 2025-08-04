b19a9e55921738dd1bdb4c0e886b11b9ea6c87b3
pranay7979 <pranayingle7979@gmail.com>
2025-08-04 11:11:59 +0530

    Changed mongo URL

--- B/db.js
+++ B/db.js
@@ -1,8 +1,10 @@
 // db.js
 const mongoose = require('mongoose');
+const mongoURI = process.env.MONGODB_URI;
+
 async function connectToMongo() {
     try {
-        await mongoose.connect(MONGODB_URI);
+        await mongoose.connect(mongoURI);
         console.log('Successfully connected to MongoDB');
     } catch (err) {
         console.error('Error connecting to MongoDB:', err);
